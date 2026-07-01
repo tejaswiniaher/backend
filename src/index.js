c23f78529d9c042223f521584c6d488dba2c501f
tejaswiniaher <tejaswiniaher905@gmail.com>
2026-07-01 14:12:34 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,20 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);      
+    })
+})
+.catch((err) => {
+    console.log("MONGO bg connection failed !!!", err);
+    
+})
 
 
 
