070b449ec4b8ab221835de67b4053091022bed9f
tejaswiniaher <tejaswiniaher905@gmail.com>
2026-07-04 08:51:45 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -20,6 +20,12 @@ app.use(express.urlencoded({
 }))
 
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+
+// route declaration
+app.use("/api/v1/users", userRouter)
 export { app }
